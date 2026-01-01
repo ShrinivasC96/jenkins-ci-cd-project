5f6a8e85c9ed7689b125afa6bb0230090ee4078b
ShrinivasC96 <150146654+ShrinivasC96@users.noreply.github.com>
2026-01-01 11:47:27 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(8, App.add(5, 3));
+    }
+}
